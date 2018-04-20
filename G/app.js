a83880ab038cefea68d6da0038e59ead80f18c57
Ponlawith Annet <ponlawitaunn@gmail.com>
2018-04-20 14:20:40 +0200

    style and fix comment

--- G/app.js
+++ G/app.js
@@ -9,19 +9,17 @@ function renderListOfProduct() {
     });
 
     for (let product of products) {
-        let $product = $(`<div style="padding: 1em">
+        let $product = $(`<div id="container">
                 <div><span><strong>Post Id: </strong></span>${product.postId}</div>
                 <div><span><strong>ID: </strong></span>${product.id}</div>
                 <div><span><strong>Name: </strong></span>${product.name}</div>
                 <div><span><strong>Likes: </strong></span>${product.QuantityLikes}</div>
                 <div style="margin-bottom: 1em"><span><strong>E-mail: </strong></span>${product.email}</div>
-                <div><button style="margin-bottom: 1em">See comments</button><div hidden="hidden"><span><strong>Comment: </strong></span>${product.body}</div></div>
+                <div><button style="margin-bottom: 1em">See comment</button><div id="comment" hidden="hidden"><span><strong>Comment: </strong></span>${product.body}</div></div>
             </div>`);
         $product.appendTo($listOfProduct);
     }
 }
-
-
 
 $.getJSON( "https://jsonplaceholder.typicode.com/posts/1/comments", function( data ) {
     products = data;
